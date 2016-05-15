fc134fcc71a013ed7794cb722a913c28d928b928
Daniel King <damaki.gh@gmail.com>
2016-05-15 11:34:35 -0300

    Fix some naming issues found by the scripts

--- scripts/generate_errors.pl
+++ scripts/generate_errors.pl
@@ -31,9 +31,9 @@ my $error_format_file = $data_dir.'/error.fmt';
 
 my @low_level_modules = qw( AES ARC4 ASN1 BASE64 BIGNUM BLOWFISH
                             CAMELLIA CCM CMAC CTR_DRBG DES
-                            ENTROPY GCM HMAC_DRBG MD2 MD4 MD5
-                            NET OID PADLOCK PBKDF2 RIPEMD160
-                            SHA1 SHA256 SHA512 THREADING XTEA );
+                            ENTROPY GCM HMAC_DRBG KECCAKF KECCAK_SPONGE
+                            MD2 MD4 MD5 NET OID PADLOCK PBKDF2 RIPEMD160
+                            SHA1 SHA256 SHA3 SHAKE SHA512 THREADING XTEA );
 my @high_level_modules = qw( CIPHER DHM ECP MD
                              PEM PK PKCS12 PKCS5
                              RSA SSL X509 );
@@ -80,6 +80,7 @@ while (my $line = <GREP>)
     $module_name = "BIGNUM" if ($module_name eq "MPI");
     $module_name = "CTR_DRBG" if ($module_name eq "CTR");
     $module_name = "HMAC_DRBG" if ($module_name eq "HMAC");
+    $module_name = "KECCAK_SPONGE" if ($module_name eq "KECCAK");
 
     my $define_name = $module_name;
     $define_name = "X509_USE,X509_CREATE" if ($define_name eq "X509");
