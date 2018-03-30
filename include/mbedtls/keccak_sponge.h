Mbed-TLS/mbedtls

Assign error codes according to the policy in error.h
This change assigns the error codes for Keccak-f[1600],
Keccak-Sponge, SHA-3, and SHAKE according to the policy
defined in error.h

A gap is left in the error code allocations for the ChaCha20/Poly1305
error codes added in the branch damaki:chacha20 in commit a2cb9fd to
avoid clashes when these two pull requests are merged (assuming they
are both accepted).

include/mbedtls/keccak_sponge.h
@@ -39,9 +39,9 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_KECCAK_SPONGE_BAD_INPUT_DATA -0x0081 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_KECCAK_SPONGE_NOT_SETUP      -0x0082 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_KECCAK_SPONGE_BAD_STATE      -0x0083 /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_KECCAK_SPONGE_BAD_INPUT_DATA -0x0021 /**< Invalid input parameter(s). */
+#define MBEDTLS_ERR_KECCAK_SPONGE_NOT_SETUP      -0x0023 /**< mbedtls_keccak_sponge_starts has not been called. */
+#define MBEDTLS_ERR_KECCAK_SPONGE_BAD_STATE      -0x0025 /**< Requested operation cannot be performed with the current context state. */
 
 typedef struct
 {

include/mbedtls/keccakf.h
@@ -40,7 +40,7 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_KECCAKF_BAD_INPUT_DATA -0x0091 /**< Invalid input parameter(s). */
+#define MBEDTLS_ERR_KECCAKF_BAD_INPUT_DATA -0x0019 /**< Invalid input parameter(s). */
 
 
 #define MBEDTLS_KECCAKF_STATE_SIZE_BITS  ( 1600U )

include/mbedtls/sha3.h
@@ -40,9 +40,9 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA   -0x00A1 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_SHA3_BAD_NOT_STARTED  -0x00A2 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_SHA3_BAD_STATE        -0x00A3 /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA   -0x0027 /**< Invalid input parameter(s). */
+#define MBEDTLS_ERR_SHA3_BAD_NOT_STARTED  -0x0029 /**< mbedtls_keccak_sponge_starts has not been called. */
+#define MBEDTLS_ERR_SHA3_BAD_STATE        -0x002B /**< Requested operation cannot be performed with the current context state. */
 
 typedef enum
 {

include/mbedtls/shake.h
@@ -40,9 +40,9 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x00B1 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_SHAKE_BAD_NOT_STARTED  -0x00B2 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_SHAKE_BAD_STATE        -0x00B3 /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x002D /**< Invalid input parameter(s). */
+#define MBEDTLS_ERR_SHAKE_BAD_NOT_STARTED  -0x002F /**< mbedtls_keccak_sponge_starts has not been called. */
+#define MBEDTLS_ERR_SHAKE_BAD_STATE        -0x0031 /**< Requested operation cannot be performed with the current context state. */
 
 typedef enum
 {

polarssl/polarssl

include/mbedtls/keccak_sponge.h
@@ -39,9 +39,9 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_KECCAK_SPONGE_BAD_INPUT_DATA -0x0081 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_KECCAK_SPONGE_NOT_SETUP      -0x0082 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_KECCAK_SPONGE_BAD_STATE      -0x0083 /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_KECCAK_SPONGE_BAD_INPUT_DATA -0x0056 /**< Invalid input parameter(s). */
+#define MBEDTLS_ERR_KECCAK_SPONGE_NOT_SETUP      -0x0058 /**< mbedtls_keccak_sponge_starts has not been called. */
+#define MBEDTLS_ERR_KECCAK_SPONGE_BAD_STATE      -0x005A /**< Requested operation cannot be performed with the current context state. */
 
 typedef struct
 {

include/mbedtls/keccakf.h
@@ -40,7 +40,7 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_KECCAKF_BAD_INPUT_DATA -0x0091 /**< Invalid input parameter(s). */
+#define MBEDTLS_ERR_KECCAKF_BAD_INPUT_DATA -0x0054 /**< Invalid input parameter(s). */
 
 
 #define MBEDTLS_KECCAKF_STATE_SIZE_BITS  ( 1600U )

include/mbedtls/sha3.h
@@ -40,9 +40,9 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA   -0x00A1 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_SHA3_BAD_NOT_STARTED  -0x00A2 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_SHA3_BAD_STATE        -0x00A3 /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA   -0x0055 /**< Invalid input parameter(s). */
+#define MBEDTLS_ERR_SHA3_BAD_NOT_STARTED  -0x0057 /**< mbedtls_keccak_sponge_starts has not been called. */
+#define MBEDTLS_ERR_SHA3_BAD_STATE        -0x0059 /**< Requested operation cannot be performed with the current context state. */
 
 typedef enum
 {

include/mbedtls/shake.h
@@ -40,9 +40,9 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x00B1 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_SHAKE_BAD_NOT_STARTED  -0x00B2 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_SHAKE_BAD_STATE        -0x00B3 /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x005B /**< Invalid input parameter(s). */
+#define MBEDTLS_ERR_SHAKE_BAD_NOT_STARTED  -0x005D /**< mbedtls_keccak_sponge_starts has not been called. */
+#define MBEDTLS_ERR_SHAKE_BAD_STATE        -0x005F /**< Requested operation cannot be performed with the current context state. */
 
 typedef enum
 {