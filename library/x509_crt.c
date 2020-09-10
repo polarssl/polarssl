850e9b01c6aec7e7daccba6603e9b61f644ae732
Eugene K <eugene.kobyakov@netfoundry.io>
2020-09-10 11:34:54 -0400

    add IP address parsing unit tests
    Signed-off-by: Eugene K <eugene.kobyakov@netfoundry.io>

--- library/x509_crt.c
+++ library/x509_crt.c
@@ -38,6 +38,8 @@
 #include "mbedtls/oid.h"
 #include "mbedtls/platform_util.h"
 
+#include "x509_invasive.h"
+
 #include <string.h>
 
 #if defined(MBEDTLS_PEM_PARSE_C)
@@ -2957,7 +2959,7 @@ find_parent:
  * parse ipv4 address from canonical string form into bytes.
  * return 0 if success, -1 otherwise
  */
-static int x509_parse_ipv4( const char *h, unsigned char *addr )
+MBEDTLS_STATIC_TESTABLE int x509_parse_ipv4( const char *h, unsigned char *addr )
 {
     int i;
     const char *strt = h;
@@ -2991,7 +2993,7 @@ static int x509_parse_ipv4( const char *h, unsigned char *addr )
  * parse ipv6 address from canonical string form into bytes.
  * return 0 if success, -1 otherwise
  */
-static int x509_parse_ipv6( const char *h, size_t hlen, unsigned char *addr )
+MBEDTLS_STATIC_TESTABLE int x509_parse_ipv6( const char *h, size_t hlen, unsigned char *addr )
 {
     const char *hend = h + hlen;
     unsigned char ip[16];
@@ -3008,9 +3010,11 @@ static int x509_parse_ipv6( const char *h, size_t hlen, unsigned char *addr )
         if( *strt != ':' ) {
             return( -1 );
         }
+        strt++;
+        colonp = ipp;
     }
 
-    while( endp < hend )
+    while( endp < hend && ipp < ip + sizeof( ip ))
     {
         /* ended with a single colon */
         if( strt == hend )
