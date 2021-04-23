be1599f1b4959e82d594fb451a059fc028493368
Gilles Peskine <Gilles.Peskine@arm.com>
2021-04-23 16:37:12 +0200

    Add storage format checks to the interface checker
    Expand abi_check.py to look for backward incompatibilities not only in
    the interface exposed to application code (and to some extent driver
    code), but also to the interface exposed via the storage format, which
    is relevant when upgrading Mbed TLS on a device with a PSA keystore.
    
    Strictly speaking, the storage format checks look for regressions in
    the automatically generated storage format test data. Incompatible
    changes that are not covered by the generated tests will also not be
    covered by the interface checker.
    
    A known defect in this commit is that the --brief output is not brief
    for storage format checks.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/scripts/generate_psa_tests.py
+++ tests/scripts/generate_psa_tests.py
@@ -405,6 +405,8 @@ class TestGenerator:
         filename = self.filename_for(basename)
         test_case.write_data_file(filename, test_cases)
 
+    # Note that targets whose name containns 'test_format' have their content
+    # validated by `abi_check.py`.
     TARGETS = {
         'test_suite_psa_crypto_not_supported.generated':
         lambda info: NotSupported(info).test_cases_for_not_supported(),
